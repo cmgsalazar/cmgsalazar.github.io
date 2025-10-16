aaff7fd41330320c16ac3d9997d4378271e2acee
Cherry Salazar <cmgsalazar@proton.me>
2025-10-16 07:19:18 -0400

    Redesign

--- functions.js
+++ functions.js
@@ -13,3 +13,18 @@ backToTopButton.onclick = function() {
     document.body.scrollTop = 0; // Safari
     document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 };
+
+// mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const tabs = document.querySelector(".tabs");
+const buttons = document.querySelectorAll(".tabs .button");
+
+menuToggle.addEventListener("click", () => {
+    tabs.classList.toggle("show");
+});
+
+document.addEventListener("click", (e) => {
+    if (!menuToggle.contains(e.target) && !tabs.contains(e.target)) {
+        tabs.classList.remove("show");
+    }
+});
